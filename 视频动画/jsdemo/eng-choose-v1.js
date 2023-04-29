635b5ebc92f65506fe152f99ccb06b71de10d39d
www <wsf@git.com>
2023-04-29 16:21:57 +0800

    0.00

--- 视频动画/jsdemo/eng-choose-v1.js
+++ 视频动画/jsdemo/eng-choose-v1.js
@@ -1,23 +1,55 @@
 import * as mo from "movy"; 
 
-let word = ["电吹风", "hair dryer"];
+/****
+单词选择 
 
-const wordGroup = mo.addGroup( );
 
-let tm = 0 ;//时间线
+***/
+let word = ["电吹风", "hair dryer","ˈher-ˌdrī(-ə)r"];
+
+
 let bg = mo.addRect({z:-1,scale:20,color: "#ffffff" });
+let tm = 0 ;//时间线
+const wordGroup = mo.addGroup( );
+//---
 //------
-let img = wordGroup.addImage('./t123456_img/电吹风.png', {x:-5,scale:3.5})
-.wipeIn  ({ ease: "slow(0.5, 0.8, false)",duration:0.9} )  ;
+let imga = wordGroup.addImage('./t123456_img/电吹风.png', {y:-1,x:-2.5,scale:2.2})
+.reveal  ({ ease: "expo.Out",duration:0.5} )  ;
+//imga.moveTo({ x: -3 , scale:2.5,duration: 0.8 ,ease:"power2.inOut"});
+tm+=1;
+//------
+let imgb = wordGroup.addImage('./t123456_img/电熨斗.jpg', {y:-1,x:2.5,scale:2.2})
+.reveal  ({ ease: "expo.Out",duration:0.5} )  ;
+//imgb.moveTo({ x: 3 , scale:2.5,duration: 0.8 ,ease:"power2.inOut"});
 
-//----
-let text = wordGroup.addText(word[0],{ y:1,  color:"#2a2a2a" })
-text.reveal( { t:tm+=1.1, duration:1.1});
+
+
+//------
+let enText =  wordGroup.addText(word[1],{ y:2.5, scale:1.5,  color:"#2a2a2a" })
+enText.reveal( {  direction:"down", duration:1.3,ease:"power4.out"});
+enText.moveTo({  y: 1.5 , scale:0.8 ,duration: 0.8 ,ease:"power2.inOut"});
+ //------
+let cnText = wordGroup.addText(word[0],{ y:2.5,  color:"#2a2a2a", scale:0.3,})
+cnText.wipeIn( {  duration:1.3,ease:"power4.out"}); //"up" | "down" | "left" | "right"
 //text.  rotateTo (10,240,0,{duration :3})
 //text.fadeOut(ea );
 
-text = wordGroup.addText(word[1],{ y:-0.5,  color:"#2a2a2a" })
-text.reveal( { t:tm+=1.1, duration:1.1});
+
+//----------
+
+imga.changeOpacity(0.1,{ t:">", duration:0.6 ,ease:"power2.in"})
+imga.changeOpacity(1,{ t:">", duration:0.6 ,ease:"power2.Out"})
+
+imga.shake2D({duration:0.2 })
+//-----
+
+imgb.changeOpacity(0.1,{ t:">", duration:0.6 ,ease:"power2.in"})
+imgb.changeOpacity(1,{ t:">", duration:0.6 ,ease:"power2.Out"})
+imgb.shake2D({duration:0.2 })
+//----
+imga.moveTo( { x:0,t:">", duration:0.6 ,ease:"power2.in"})
+imgb.changeOpacity(0,{ t:">", scale:2.5,duration:0.6 ,ease:"power2.Out"})
+//---
 
 
-wordGroup.moveTo({ x: 3 , duration: 0.8 ,ease:"power2.inOut"});
+wordGroup.moveTo({ scale:0,duration: 0.8 ,ease:"power2.inOut"});
--- 视频动画/jsdemo/eng-v1.js
+++ 视频动画/jsdemo/eng-v1.js
@@ -1,39 +1,55 @@
 import * as mo from "movy"; 
 
+/****
+单词选择 
+
+
+***/
 let word = ["电吹风", "hair dryer","ˈher-ˌdrī(-ə)r"];
-
-const wordGroup = mo.addGroup( );
-
-let tm = 0 ;//时间线
-
-let phText =  wordGroup.addText(word[2],{ y:-2,font:"zh",  color:"#2a2a2a" })
-phText.reveal( { t:tm+=1.3, duration:1.3,ease:"power4.out"});
 
 
 let bg = mo.addRect({z:-1,scale:20,color: "#ffffff" });
+let tm = 0 ;//时间线
+const wordGroup = mo.addGroup( );
+//---
 //------
-let img = wordGroup.addImage('./t123456_img/电吹风.png', {x:0,scale:4.5})
-.wipeIn  ({ ease: "slow(0.5, 0.8, false)",duration:1.1} )  ;
-
-img.moveTo({ x: -5 , scale:3,duration: 0.8 ,ease:"power2.inOut"});
+let imga = wordGroup.addImage('./t123456_img/电吹风.png', {y:-1,x:-2.5,scale:2.2})
+.reveal  ({ ease: "expo.Out",duration:0.5} )  ;
+//imga.moveTo({ x: -3 , scale:2.5,duration: 0.8 ,ease:"power2.inOut"});
 tm+=1;
-//----
-let cnText = wordGroup.addText(word[0],{ y:1,  color:"#2a2a2a" })
-cnText.reveal( { t:tm+=1.3, duration:1.3,ease:"power4.out"});
+//------
+let imgb = wordGroup.addImage('./t123456_img/电熨斗.jpg', {y:-1,x:2.5,scale:2.2})
+.reveal  ({ ease: "expo.Out",duration:0.5} )  ;
+//imgb.moveTo({ x: 3 , scale:2.5,duration: 0.8 ,ease:"power2.inOut"});
+
+
+
+//------
+let enText =  wordGroup.addText(word[1],{ y:2.5, scale:1.5,  color:"#2a2a2a" })
+enText.reveal( {  direction:"down", duration:1.3,ease:"power4.out"});
+enText.moveTo({  y: 1.5 , scale:0.8 ,duration: 0.8 ,ease:"power2.inOut"});
+ //------
+let cnText = wordGroup.addText(word[0],{ y:2.5,  color:"#2a2a2a", scale:0.3,})
+cnText.wipeIn( {  duration:1.3,ease:"power4.out"}); //"up" | "down" | "left" | "right"
 //text.  rotateTo (10,240,0,{duration :3})
 //text.fadeOut(ea );
 
-cnText.changeOpacity(0.7,{ t:">", duration:0.2 ,ease:"power2.in"})
-cnText.changeOpacity(1,{ t:">", duration:0.2 ,ease:"power2.Out"})
 
-let enText = wordGroup.addText(word[1],{ y:-0.5,  color:"#2a2a2a" })
-enText.reveal( { t:tm+=3.8, duration:1.3,ease:"power4.out"});
+//----------
 
-enText.changeOpacity(0.7,{ t:">", duration:0.2 ,ease:"power2.in"})
-enText.changeOpacity(1,{ t:">", duration:0.2 ,ease:"power2.Out"})
+imga.changeOpacity(0.1,{ t:">", duration:0.6 ,ease:"power2.in"})
+imga.changeOpacity(1,{ t:">", duration:0.6 ,ease:"power2.Out"})
+
+imga.shake2D({duration:0.2 })
+//-----
+
+imgb.changeOpacity(0.1,{ t:">", duration:0.6 ,ease:"power2.in"})
+imgb.changeOpacity(1,{ t:">", duration:0.6 ,ease:"power2.Out"})
+imgb.shake2D({duration:0.2 })
+//----
+imga.moveTo( { x:0,t:">", duration:0.6 ,ease:"power2.in"})
+imgb.changeOpacity(0,{ t:">", scale:2.5,duration:0.6 ,ease:"power2.Out"})
+//---
 
 
-// let phText =  wordGroup.addText(word[2],{ y:-2,font:"zh",  color:"#2a2a2a" })
-// phText.reveal( { t:tm+=1.3, duration:1.3,ease:"power4.out"});
-
-wordGroup.moveTo({ t:tm+=3.8, y: 6 , scale:0.01,duration: 0.8 ,ease:"power2.inOut"});
+wordGroup.moveTo({ scale:0,duration: 0.8 ,ease:"power2.inOut"});
