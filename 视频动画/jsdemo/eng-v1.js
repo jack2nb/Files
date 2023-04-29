80ef4d92abce64420d41bd18cd03329f69360609
www <wsf@git.com>
2023-04-29 09:46:38 +0800

    0.0

--- 视频动画/jsdemo/eng-v1.js
+++ 视频动画/jsdemo/eng-v1.js
@@ -2,6 +2,8 @@ import * as mo from "movy";
 /********
 完成英语 学习视频
  
+单图介绍 
+
 ******/  
 let text = mo.addText("先下载 melon吗 ", {
   scale: 0.8,
@@ -22,7 +24,7 @@ mo.addText("那就用v2ray 扫描此处二维码", {
 .wipeIn({t:3,duration:2, ease: "bounce.out"})
 .fadeOut({t:7,duration:2, ease: "expo.out"});
 
-let img = mo.addImage('11117.png' ,{t:3,scale:5} )
+let img = mo.addImage('./t123456_img/电吹风.jpg' ,{t:3,scale:5} )
 .rotateIn ()  ;
 
 img.moveTo({x:2,y:2,duration: 1, ease: "expo.inOut"})
